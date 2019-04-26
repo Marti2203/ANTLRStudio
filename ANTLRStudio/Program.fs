94c479f26ea47f9cd4e663e6c10a36347d6829a4
Martin Mirchev <marti_2203@abv.bg>
2019-04-26 22:28:32 +0300

    Display Exception Message

--- ANTLRStudio/Program.fs
+++ ANTLRStudio/Program.fs
@@ -11,7 +11,8 @@ let main argv =
     let progName = "ANTLRStudio"
     Console.Title <- progName
     use app = new Application ()
-    app.UnhandledException.Add(fun e -> printfn "%s" (e.ExceptionObject :?> Exception).StackTrace )
+    app.UnhandledException.Add(fun e -> let e = (e.ExceptionObject :?> Exception)
+                                        printfn "%s %s %s" e.Message Environment.NewLine e.StackTrace )
     use form = new Form (Title = progName, Size = Size(Screen.PrimaryScreen.Bounds.Size))
 
     setupInitialMenus app form
