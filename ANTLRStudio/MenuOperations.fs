b6f8c5d6feb7043cfce03bf1c1cb2c97c52b4e1c
Martin2203 <mirchevmartin2203@gmail.com>
2019-04-27 15:48:42 +0300

    Generate In Specfic Folder

--- ANTLRStudio/MenuOperations.fs
+++ ANTLRStudio/MenuOperations.fs
@@ -2,6 +2,9 @@
 open Eto.Forms;
 open AntlrTools;
 open FormElements;
+open System.IO
+open System
+
 let RadioMenuItem = Menu.RadioMenuItem
 let CheckMenuItem = Menu.CheckMenuItem
 let mutable private addedMenus = false
@@ -12,7 +15,27 @@ let addMenus (form:Form) (menuItems: MenuItem seq) =
 let removeMenus (form:Form) (menuItems: MenuItem seq) =
     menuItems |> Seq.iter (form.Menu.Items.Remove >> ignore)
 
-let specificMenus =
+let generateIn (form:Form) =
+    let dir = Directory.GetCurrentDirectory() // Eto.GTK changes the directory...
+    
+    use dialog = new SelectFolderDialog(Title = "Select Folder For File generation",
+                                        Directory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+    let dialogResult = dialog.ShowDialog(form.ParentWindow)
+    Directory.SetCurrentDirectory(dir) // Eto.GTK changes the directory...
+    let output directory = 
+        [{
+         Name="Output Directory Flag"; Value= true; ActiveFlag="-o";InactiveFlag=String.Empty
+         }
+         {
+         Name="Output Directory Location"; Value= true; ActiveFlag=(sprintf "\"%s\"" directory);InactiveFlag=String.Empty
+         }]
+    match dialogResult with
+    | DialogResult.Ok -> if dialog.Directory <> null 
+                         then
+                            generate file language (options |> Seq.append(output dialog.Directory))
+    | v -> printf "User pressed %O" v
+
+let createSpecificMenus (form:Form) =
     let transformCheckItem option =
         let item = CheckMenuItem(option.Name)
                    |> action (fun _ -> option.Value <- not option.Value )
@@ -24,18 +47,19 @@ let specificMenus =
                         |> Seq.map(fun (name,value) -> RadioMenuItem("Languages",name) 
                                                        |> action (fun _ -> language <- value)))
     SubMenu("Options",  options |> Seq.map transformCheckItem)
-    ActionMenuItem("Generate")  |> action (fun _ -> generate file language options)
+    ActionMenuItem("Generate")  |> action (fun _ -> generateIn form)
     ] 
     |> Seq.map makeMenu
-
+let mutable specificMenus = null
 let addSpecificMenus form =
     if not addedMenus then
         addedMenus <- true
+        specificMenus <- (createSpecificMenus form)
         addMenus form specificMenus
 let removeSpecificMenus form =
     if addedMenus then
         addedMenus <- false
-        addMenus form specificMenus
+        removeMenus form specificMenus
 let setupInitialMenus (app:Application) (form:Form) =
     let menu = new MenuBar()
     let menus = [
