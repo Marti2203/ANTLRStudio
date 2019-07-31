61da83681dba9b0754fbcf95125191fa299f9677
Martin2203 <mirchevmartin2203@gmail.com>
2019-07-31 22:05:27 +0300

    Adding AvaloniaEdit

---
+++ AvaloniaEdit/src/AvaloniaEdit/CodeCompletion/PopupWithCustomPosition.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaEdit.CodeCompletion
+{
+    internal class PopupWithCustomPosition : Popup
+    {
+        public static readonly AvaloniaProperty<Point> PositionProperty =
+            AvaloniaProperty.Register<PopupWithCustomPosition, Point>(nameof(Position));
+
+        public PixelPoint Position { get; set; }
+
+        protected override PixelPoint GetPosition()
+        {
+            return Position;
+        }
+    }
+}
