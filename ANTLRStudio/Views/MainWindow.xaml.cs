e34d1f1f7a832a059bcb8721cb9d3987c9d202a0
Plamen Berberov <plamen.berberov@gmail.com>
2019-07-31 23:09:36 +0300

    AvaloniaEdit init

--- ANTLRStudio/Views/MainWindow.xaml.cs
+++ ANTLRStudio/Views/MainWindow.xaml.cs
@@ -1,14 +1,29 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using AvaloniaEdit;
+using AvaloniaEdit.CodeCompletion;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.Editing;
+using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.Rendering;
 
 namespace ANTLRStudio.Views
 {
     public class MainWindow : Window
     {
+        private readonly TextEditor _textEditor;
         public MainWindow()
         {
             InitializeComponent();
+
+            _textEditor = this.FindControl<TextEditor>("Editor");
+            _textEditor.Background = Brushes.Aquamarine;
+            _textEditor.ShowLineNumbers = true;
+            _textEditor.TextArea.Foreground = Brushes.Black;
         }
 
         private void InitializeComponent()
