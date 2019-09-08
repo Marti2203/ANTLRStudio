cf53021902ef58d1185facf918e49deaecbdcb7a
Plamen Berberov <plamen.berberov@gmail.com>
2019-09-09 02:58:22 +0300

    StructuredCanvas control created and used for Diagram panel

---
+++ ANTLRStudio.Drawing/StructuredCanvas.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace ANTLRStudio.Drawing
+{
+    class StructuredCanvas : Canvas
+    {
+        public StructuredCanvas()
+        {
+
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var children = Children;
+            double previousChildSize = 0.0;
+            var spacing = 10;
+
+            //
+            // Arrange and Position Children.
+            //
+            for (int i = 0, count = children.Count; i < count; ++i)
+            {
+                var child = children[i];
+
+                if (child == null)
+                { continue; }
+
+                SetLeft(child as AvaloniaObject, previousChildSize);
+                previousChildSize += child.DesiredSize.Width;
+                previousChildSize += spacing;
+            }
+
+            base.ArrangeOverride(finalSize);
+
+            return finalSize;
+        }
+    }
+}
